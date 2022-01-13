5ceffe99d1ecdf98897b775c805e954099cda2ee
Asus <louisfuego03@gmail.com>
2022-01-13 18:50:08 +0800

    Coffee Button now makes a sound

--- app/src/main/java/com/example/fuego_myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/fuego_myfirstapp/MainActivity.java
@@ -4,10 +4,17 @@ package com.example.fuego_myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button2;
 
     MediaPlayer player;
     @Override
@@ -18,7 +25,40 @@ public class MainActivity extends AppCompatActivity {
         player = new MediaPlayer();
         player = MediaPlayer.create(this,R.raw.ensolarado);
 
-                player.start();
+        Button one = (Button) this.findViewById(R.id.button2);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.ain_soph_aur);
+        one.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mp.start();
+            }
+        });
+
+
+
+
+        player.start();
+
+
+        }
+
+
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        if (player.isPlaying()) {
+            player.pause();
+
+        }
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        player.start();
 
     }
 }
